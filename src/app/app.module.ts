d89aa64f617e9909f97e3e5631f42b7a2f6c194d
Brian Phillips <brian.phillips@qq.com>
2017-09-09 10:36:03 +0800

    init commit, firebase installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA6M4jHmUuwQO1tz_Lud0MHK4ydDY1W1MY',
+  authDomain: 'pepper-e07b1.firebaseapp.com',
+  databaseURL: 'https://pepper-e07b1.firebaseio.com',
+  projectId: 'pepper-e07b1',
+  storageBucket: '',
+  messagingSenderId: '89721359338'
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
